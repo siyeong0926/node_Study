f1468498b91a1e47078b920f46c4d78107822bb5
김시영 <siyeong@naver.com>
2023-11-02 01:17:23 +0900

    api 분리 연습 끝

---
+++ routes/detail.js
@@ -0,0 +1,37 @@
+const {ObjectId} = require("mongodb");
+const connectDB = require("../database");
+const router = require('express').Router()
+
+let db;
+connectDB.then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('forum')
+}).catch((err)=>{
+    console.log(err)
+})
+
+router.get("/detail/:id", async (req,res)=>{
+    try {
+
+        // MongoDB의 'post' 컬렉션에서 _id 필드가 URL 파라미터로 받은 id와 일치하는 문서를 찾습니다.
+        // URL 파라미터로 받은 id는 문자열이므로, MongoDB의 ObjectId로 변환해줘야 합니다.
+        let result = await db.collection('post').findOne({ _id : new ObjectId(req.params.id)})
+        console.log(new ObjectId(req.params.id))
+
+        if (result == null){
+            res.status(400).send('이상한 url 입력했음')
+        }
+
+        res.render('detail.ejs',{
+            titleName : result,
+            contentName : result,
+            imgURL : result
+        })
+
+    }catch (e){
+        console.log(e)
+        res.status(400).send('이상한 url 입력함')
+    }
+})
+
+module.exports = router
--- routes/edit.js
+++ routes/edit.js
@@ -1,1 +1,48 @@
-        const router = require('express').Router()
+const {ObjectId} = require("mongodb");
+const connectDB = require("../database");
+const router = require('express').Router()
+
+let db;
+connectDB.then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('forum')
+}).catch((err)=>{
+    console.log(err)
+})
+
+router.get("/edit/:id", async (req,res)=>{
+
+    let result = await db.collection('post').findOne({ _id : new ObjectId(req.params.id)})
+
+    console.log(new ObjectId(req.params.id))
+
+    res.render("edit.ejs", {result})
+})
+
+router.put("/edit", async (req,res)=>{
+
+    try {
+        let result = await db.collection('post').updateOne(
+            {_id : new ObjectId(req.body.id )},
+            {$set : { title : req.body.title, content : req.body.content }})
+
+        console.log(req.body)
+        res.redirect('/list')
+
+        if (result == null){
+            res.status(400).send('문제있는 url')
+        }
+    }catch (e){
+        console.log(e)
+        res.status(400).send('문제되는 URL임')
+
+    }
+
+});
+
+module.exports = router
+// app.put('/edit', async (req,res)=>{
+//     await db.collection('post').updateOne(
+//         { _id : 1 },
+//         {$inc : {like : -2}})
+// })
